599e6b8fa82ac5259ca29333ccb485c782f9fa56
Mu57Di3 <mu57di3@gmail.com>
2015-06-02 23:59:17 +0300

    Пока тестовый далеко не финальный вариант инициализации приложения

--- src/main.js
+++ src/main.js
@@ -2,17 +2,32 @@
  * Created by Mu57Di3 on 01.06.2015.
  */
 
-var jQuery = require('jquery');
-var _   = require('underscore');
-var Backbone = require('backbone');
-Backbone.$ = jQuery;
-var Marionette = require('backbone.marionette');
-
-
 var MyApp = Marionette.Application.extend({
     initialize:function (){
-        console.log('1111');
+
+    },
+
+    events:{
+        'start':'startHandler'
+    },
+
+    startHandler: function(){
+
     }
+
 });
 
-var app = new MyApp({container: '#app'});
+var app = new MyApp();
+app.addRegions({
+    main: "#app"
+});
+app.on('start',function(){
+    var vEditor = require('./ctrl/editor');
+    this.getRegion('main').show(vEditor);
+});
+app.start();
+
+
+
+
+
