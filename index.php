bcecbae05820ceba202aa50dc743f1377eb3a1a1
Mu57Di3 <mu57di3@gmail.com>
2015-06-04 00:26:05 +0300

    Динамические роуты. Вынесены контроллеры в отдельные файлы

--- index.php
+++ index.php
@@ -14,8 +14,22 @@ $app->get('/',function(){
     echo file_get_contents(__DIR__.'/templates/index.html');
 });
 
-$app->get('/app',function(){
-    echo 'foo';
+$app->get('/:route',function ($route) use ($app){
+    $path = sprintf('%s/controllers/%s.php',__DIR__,$route);
+    if (file_exists($path)) {
+        include $path;
+    } else {
+        $app->notFound();
+    }
+});
+
+$app->post('/:route',function ($route) use ($app){
+    $path = sprintf('%s/controllers/%s.php',__DIR__,$route);
+    if (file_exists($path)) {
+        include $path;
+    } else {
+        echo json_encode(array('status'=>'error','data'=>'CONTROLLER_NOT_EXIST'));
+    }
 });
 
 $app->run();
