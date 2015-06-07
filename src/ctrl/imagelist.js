f01ba1d8ac17e66062c6f69468b87e2a28e03d94
Mu57Di3 <mu57di3@gmail.com>
2015-06-07 23:38:38 +0300

    Добавленны обработчики кнопок редактирования и удаления

--- src/ctrl/imagelist.js
+++ src/ctrl/imagelist.js
@@ -1,4 +1,5 @@
 /**
+ * Контроллер скиска файлов
  * Created by Bender on 04.06.2015.
  */
 
@@ -7,16 +8,79 @@ var ImageItemView = Marionette.ItemView.extend({
     template:'templates/image.twig',
 
     events:{
-
-    },
-
-    initialize:function (){
-
+        'click #bDelete':'removeImage',
+        'click #bEdit':'editImage'
     },
 
     onBeforeRender: function (){
         this.templateHelpers = {};
         this.templateHelpers = this.model.attributes;
+    },
+
+    removeImage:function(){
+        var id = this.model.get('name');
+        var edit_key = prompt("Введите ключ редактирования данного изображения");
+
+        if (edit_key != null && edit_key.length>0) {
+            var sendData = {
+                name:id,
+                edit_key:edit_key
+            };
+
+            $.ajax({
+                url:'http://tz.mu57di3.org/delImage',
+                dataType:'json',
+                type:'GET',
+                data:sendData
+            }).success(function(input){
+                if (input.status == 'ok'){
+                    app.ImagesCollection.delImage(id);
+                } else {
+                    alert('Неправильный ключ редактирования');
+                }
+            }).error(function(data){
+                console.log(data);
+            });
+        }
+    },
+
+    editImage:function(){
+        var id = this.model.get('name');
+        var edit_key = prompt("Введите ключ редактирования данного изображения");
+
+        if (edit_key != null && edit_key.length>0) {
+            var sendData = {
+                name:id,
+                edit_key:edit_key
+            };
+
+            $.ajax({
+                url:'http://tz.mu57di3.org/checkkey',
+                dataType:'json',
+                type:'GET',
+                data:sendData
+            }).success(function(input){
+                if (input.status == 'ok'){
+                    var Modal = require('./popup'),
+                        editor = require('./editor');
+                    app.MainView.getRegion('modal').show(new Modal({
+                        v:new editor({
+                            name:id,
+                            edit_key:edit_key
+                        }),
+                        tpl:{
+                            title:'Редактирование изображения '+id,
+                            button:''
+                        },
+                        success:false
+                    }));
+                } else {
+                    alert('Неправильный ключ редактирования');
+                }
+            }).error(function(data){
+                console.log(data);
+            });
+        }
     }
 
 
@@ -25,7 +89,7 @@ var ImageItemView = Marionette.ItemView.extend({
 var ImageList  = Marionette.CollectionView.extend({
     tagName:'table',
     childView: ImageItemView,
-    className:'table table-striped table-bordered',
+    className:'table table-striped',
 
     onShow: function(){
         console.log('Imagelist show');
